7f0fadb787cda5da9a067b07aa456b59d42b191d
PandaTim <jintimmy1130@gmail.com>
2019-04-11 23:13:20 +0900

    first commit

--- depth/census.py
+++ depth/census.py
@@ -53,9 +53,9 @@ def drawFigure(loc, img, label):
     plt.title(label), plt.xticks([]), plt.yticks([])
 
 def depth_hazy_clear(hazy_image, clear_image, a, beta):
-    dividend_r = -(np.log(hazy_image[:,:,0] - a[0]) - np.log(clear_image[:,:,0] - a[0])) / beta
-    dividend_g = -(np.log(hazy_image[:,:,1] - a[1]) - np.log(clear_image[:,:,1] - a[1])) / beta
-    dividend_b = -(np.log(hazy_image[:,:,2] - a[2]) - np.log(clear_image[:,:,2] - a[2])) / beta
+    dividend_r = -(np.log(hazy_image[:,:,0] - a) - np.log(clear_image[:,:,0] - a)) / beta
+    dividend_g = -(np.log(hazy_image[:,:,1] - a) - np.log(clear_image[:,:,1] - a)) / beta
+    dividend_b = -(np.log(hazy_image[:,:,2] - a) - np.log(clear_image[:,:,2] - a)) / beta
     depth = dividend_b + dividend_g + dividend_r / 3
     plt.imshow(depth, cmap="gray")
     plt.show()
@@ -63,18 +63,17 @@ def depth_hazy_clear(hazy_image, clear_image, a, beta):
 # 사진을 받아서, 대략적 depth 분포를 볼 것이다.
 H = cv2.imread('HEB_Bing_091_fake_A.png')
 D = cv2.imread('HEB_Bing_091_real_B.png')
-
+mask_size = 5
 # 사진의 dark channel prior를 구한다. 논문에서는 patch size를 15x15로 실험했다.
-image_dark_channel = get_dark_channel(H, 7)
+image_dark_channel = get_dark_channel(H, mask_size)
 
 # A를 구할 때, 상위 0.1% 안에 드는 픽셀들을 고른다.
 atmosphere_light = get_atmosphere(H, image_dark_channel, 0.01)
 
 # Transmission rate (T(x))를 구한다. 논문에서는 omega 값을 0.95로 설정했다.
-transmission_rate = get_transmission(H, atmosphere_light, 0.95, 7)
+transmission_rate = get_transmission(H, atmosphere_light, 0.95, mask_size)
 
 # d(x)를 구한다. 산란 계수를 정해야 한다.
-a = depth_hazy_clear(H, D, atmosphere_light, 0.4)
 '''
 drawFigure((2, 3, 1), get_depth(transmission_rate, 0.4), "0.4")
 drawFigure((2, 3, 2), get_depth(transmission_rate, 0.6), "0.6")
@@ -82,11 +81,10 @@ drawFigure((2, 3, 3), get_depth(transmission_rate, 0.8), "0.8")
 drawFigure((2, 3, 4), get_depth(transmission_rate, 1.0), "1.0")
 drawFigure((2, 3, 5), get_depth(transmission_rate, 1.2), "1.2")
 drawFigure((2, 3, 6), get_depth(transmission_rate, 1.4), "1.4")
-depth = get_depth(transmission_rate, 0.4)
-depth = np.concatenate((depth, get_depth(transmission_rate, 0.6)))
-depth = np.concatenate((depth, get_depth(transmission_rate, 0.8)))
-plt.imshow(depth, cmap="gray")
 '''
+#depth = depth_hazy_clear(H, D, 1.0, 1.6)
+depth = get_depth(transmission_rate, 0.8)
+plt.imshow(depth, cmap="gray")
 plt.show()
 '''
 cv2.imshow('test', depth)
